706d3403c49617110a3a30cd18f75d9f835e98fa
Arsly Mwamwaya <arslymwamwaya9@gmail.com>
2026-08-09 00:13:10 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
+function toggleAbout() {
+  const content = document.getElementById("aboutContent");
+  content.classList.toggle("show");
+}
 
+function toggleInfo(id) {
+  const info = document.getElementById(id);
+  info.classList.toggle("show");
+}
